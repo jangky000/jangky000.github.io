e018e46fece6bc9d9105ba98bdb018b4db2d85eb
jangky000 <jangky000@mme.dongguk.edu>
2022-01-05 10:55:28 +0900

    FIX getPosts script

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require('./scripts/getPosts');
       require('./scripts/sitemap-generator');
     }
     return config;
