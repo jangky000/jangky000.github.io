224962ce090644f18ff78583388f86acd17a1826
jangky000 <jangky000@mme.dongguk.edu>
2022-03-06 23:14:28 +0900

    UPDATE component directory 구조

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { StyledHomeLayout } from '@styles/home/style';
-import { Footer } from '@components/organisms/Footer';
-import { Header } from '@components/organisms/Header';
+import { Footer } from '@components/Footer';
+import { Header } from '@components/Header';
 import styles from '@styles/Index.module.scss';
 import postlist from '@jsons/posts.json';
 import { ReactElement } from 'react';
