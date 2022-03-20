145b74defcae03dc8479f2545518eeafe6dbc917
jangky000 <jangky000@mme.dongguk.edu>
2022-03-20 14:56:09 +0900

    UPDATE anchor, code style

--- styles/posts/style.ts
+++ styles/posts/style.ts
@@ -23,10 +23,19 @@ export const StyledPostLayout = styled.main`
   .post-contents {
     a {
       color: ${({ theme }) => theme.colors.dark};
+      text-decoration: underline;
       &:hover {
         color: #000;
         cursor: pointer;
       }
+    }
+
+    code {
+      background: rgba(135, 131, 120, 0.15);
+      color: #eb5757;
+      border-radius: 3px;
+      font-size: 85%;
+      padding: 0.2em 0.4em;
     }
   }
 `;
