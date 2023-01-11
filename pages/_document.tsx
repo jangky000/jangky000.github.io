bb633a07c85b4dab56d523f73aa7e37338dbfdb1
jangky000 <jangky000@mme.dongguk.edu>
2023-01-11 18:10:24 +0900

    Add naver site verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
-import { Children } from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Children } from 'react';
 
 export default class MyDocument extends Document {
   render() {
@@ -10,6 +10,10 @@ export default class MyDocument extends Document {
           <meta
             name="google-site-verification"
             content="DPsgjCONuCmXo7g20zSkClsSwOef21VzS4Ihvwgjna0"
+          />
+          <meta
+            name="naver-site-verification"
+            content="e5e67bc35f28f45a7fcf3ee3ea2f5b081ea5ad87"
           />
         </Head>
         <body>
