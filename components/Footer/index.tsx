19ad4092268b59cc7af12cc9f377c57c66b7c3e3
jangky000 <jangky000@mme.dongguk.edu>
2023-01-19 11:21:45 +0900

    Rename author

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,11 @@
+import { CONFIGS } from 'configs';
 import { style } from './style';
 
 export const Footer = () => (
   <>
     <footer>
       <div className="copyright">
-        Copyright 2021. jangky000 all rights reserved.
+        Copyright 2021. {CONFIGS.author} all rights reserved.
       </div>
       <style jsx>{style}</style>
     </footer>
--- configs.ts
+++ configs.ts
@@ -3,5 +3,5 @@ export const CONFIGS = {
   description: 'FE 개발자로 일하면서 겪은 경험담을 정리합니다.',
   url: 'https://jangky000.github.io/',
   github: 'https://github.com/jangky000',
-  author: 'jangky000',
+  author: 'FE는 철',
 } as const;
