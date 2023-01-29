3c459a50a365d8ef21d96dd943f5894f40da1530
jangky000 <jangky000@mme.dongguk.edu>
2023-01-29 21:31:21 +0900

    Add rss parser

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,20 @@ import { Header } from '@components/Header';
 import PostList from '@components/PostList';
 import Seo from '@components/Seo';
 import { useTheme } from '@emotion/react';
+import { getFeNewsRss } from '@libs/rss';
 import { Tab } from '@material-ui/core';
 import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 import { StyledHomeLayout } from '@styles/home/style';
 import styles from '@styles/Index.module.scss';
 import usePostList from 'hooks/usePostList';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { animated, useSpring } from 'react-spring';
 
-const Home = (): ReactElement => {
+interface Props {
+  rss: any;
+}
+
+const Home = ({ rss }: Props): ReactElement => {
   const theme = useTheme();
   const fadeInStyle = useSpring({ opacity: 1, from: { opacity: 0 } });
   const postList = usePostList();
@@ -25,6 +30,10 @@ const Home = (): ReactElement => {
   const handleChange = (event: any, newValue: any) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    console.log(rss);
+  }, [rss]);
 
   return (
     <div className={styles.container}>
@@ -70,5 +79,14 @@ const Home = (): ReactElement => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const feNewsRss = await getFeNewsRss();
+  const items = feNewsRss.items ?? [];
+
+  return {
+    props: { rss: items },
+  };
+}
 
 export default Home;
