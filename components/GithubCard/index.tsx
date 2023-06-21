95d4bd15feb685d93865d22315cd44fc54db6a4b
jangky000 <jangky000@mme.dongguk.edu>
2023-06-21 17:04:01 +0900

    Fix external links

--- components/GithubCard/index.tsx
+++ components/GithubCard/index.tsx
@@ -9,17 +9,19 @@ function GithubCard({ url }: Props) {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   return (
-    <GithubLink href={url} target="_blank">
-      <GithubIcon>
-        <img
-          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-          alt="github-icon"
-        />
-        <div className="title">GitHub</div>
-      </GithubIcon>
-      <div className="url">
-        {isMobile ? url.replace('blob/master/issues/', '') : url}
-      </div>
+    <GithubLink href={url} passHref>
+      <a>
+        <GithubIcon>
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="github-icon"
+          />
+          <div className="title">GitHub</div>
+        </GithubIcon>
+        <div className="url">
+          {isMobile ? url.replace('blob/master/issues/', '') : url}
+        </div>
+      </a>
     </GithubLink>
   );
 }
