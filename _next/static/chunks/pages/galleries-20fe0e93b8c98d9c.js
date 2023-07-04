ce9a2386cf906d894ace130ed486214185fd9e29
jangky000 <jangky000@mme.dongguk.edu>
2023-07-04 11:11:07 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/chunks/pages/galleries-20fe0e93b8c98d9c.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[995],{7473:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/galleries",function(){return u(69770)}])}},function(n){n.O(0,[296,689,670,886,857,107,770,774,888,179],function(){return n(n.s=7473)}),_N_E=n.O()}]);
+//# sourceMappingURL=galleries-20fe0e93b8c98d9c.js.map
